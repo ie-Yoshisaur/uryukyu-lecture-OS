f5bcab58fbd48604c9df45fd2c0288c16e714963
e205723 <e205723@ie.u-ryukyu.ac.jp>
2022-02-15 05:16:21 +0900

    chagne the multicast address

---
+++ 7.x/7.3/Source.java
@@ -0,0 +1,20 @@
+import java.net.InetAddress;
+import java.net.DatagramPacket;
+import java.net.MulticastSocket;
+
+public class SendMulticast{
+    public static void main(String[] args) {
+        try {
+            int port = 8888;
+            String dataToSend = "Hello, World!";
+            byte[] data = dataToSend.getBytes("UTF-8");
+            InetAddress address = InetAddress.getByName("239.1.14.126");
+            DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+            MulticastSocket socket = new MulticastSocket();
+            socket.send(packet);
+            socket.close();
+	} catch (Exception e) {
+            System.err.println(e);
+	}
+    }
+}
